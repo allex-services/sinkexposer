71d64c25f3dcea415bac01f1fade7b02126dd5dc
Veljko Popovic <veljko@888.rs>
2015-12-17 14:51:20 +0100

    cleaning up ...

--- users/usercreator.js
+++ users/usercreator.js
@@ -8,23 +8,30 @@ function createUser(execlib, ParentUser) {
 
   function User(prophash) {
     ParentUser.call(this, prophash);
+    this.osDestroyedListener = null;
     var os = this.__service.state.get('outerSink'),
       p = this.purge.bind(this);
     if (!(os && os.destroyed)) {
       console.log('*SinkExposer created with no outerSink in __service');
       lib.runNext(p);
     } else {
-      os.destroyed.attach(p);
+      this.osDestroyedListener = os.destroyed.attach(p);
     }
   }
   
   ParentUser.inherit(User, require('../methoddescriptors/user'), [/*visible state fields here*/]/*or a ctor for StateStream filter*/);
   User.prototype.__cleanUp = function () {
+    if(this.osDestroyedListener) {
+      this.osDestroyedListener.destroy();
+    }
+    this.osDestroyedListener = null;
     ParentUser.prototype.__cleanUp.call(this);
   };
 
   User.prototype.purge = function () {
+    console.log('remote outerSink is down');
     if (!this.destroyed) {
+      console.log('but me ded already');
       return;
     };
     console.log(this.get('name'), this.__service.subSinkName, 'purging self');
